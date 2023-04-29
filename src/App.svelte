d11926c2c395c287f0ff03ac010e519989c1bae1
Amaral Krichman <amaralkrichman@gmail.com>
2023-04-29 18:00:42 -0300

    update css, add font

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
   import { ResourcesInfo } from '$features';
-
   import IncarnationCycle from '$features/incarnation-period';
   import MainAction from '$features/main-action.svelte';
   import UpgradesWidget from '$features/upgrades';
   import BuildingsWidget from '$features/buildings';
 </script>
 
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500&display=swap" rel="stylesheet">
+</svelte:head>
+
 <header>
   <h1>Karma Clicker</h1>
   <ResourcesInfo />
-
 </header>
 <main>
   <div class="container">
