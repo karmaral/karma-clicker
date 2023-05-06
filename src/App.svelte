0f5abfc4c579a1bdb116a721ea0dfcfe8f850fab
Amaral Krichman <amaralkrichman@gmail.com>
2023-05-06 03:04:44 -0300

    chore: easier font

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,7 @@
 <svelte:head>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Encode+Sans:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500&display=swap" rel="stylesheet">
 </svelte:head>
 
 <header>
