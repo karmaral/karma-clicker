f5b9f99ffc5611ea99fb2ea7574c11821e365864
Amaral Krichman <amaralkrichman@gmail.com>
2023-05-15 21:15:57 -0300

    feat: add tooltips

--- src/features/ui/index.ts
+++ src/features/ui/index.ts
@@ -1,11 +1,13 @@
 import ProgressBar from './progress-bar.svelte';
 import CurrencySymbol from './currency-symbol.svelte';
+import InfoTooltip from './info-tooltip.svelte';
 import ResourceQuantity from './resource-quantity.svelte';
 import BuyModeSwitcher from './buy-mode-switcher.svelte';
 
 export { 
   ProgressBar,
   CurrencySymbol,
+  InfoTooltip,
   ResourceQuantity,
   BuyModeSwitcher,
 };
